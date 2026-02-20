26b10f8ea79e71436372c7c3a4c694de94e7ce25
kuldeep-01 <kuldeepsinghdlw10072002@gmail.com>
2026-02-21 02:12:07 +0530

    minor bugs

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import pandas as pd
 app=Flask(__name__)
 ## Load the model
 regmodel=pickle.load(open('regmodel.pkl','rb'))
-scalar=pickle.load(open('scaling.pkl','rb'))
+scalar=pickle.load(open('scaler.pkl','rb'))
 @app.route('/')
 def home():
     return render_template('home.html')
